51463a36c9e9f50e0769983c5c589a285dca12e2
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2024-11-12 17:55:48 -0400

    versao em swift, porem entrada nao formatada de acordo com padrao judge Beecrowd

--- beginner/swift/debug.swift
+++ beginner/swift/debug.swift
@@ -1,6 +1,1 @@
 import Foundation
-
-//input
-var number:Int = Int(readLine()!)!
-
-print(number)
--- beginner/swift/macPronalts.swift
+++ beginner/swift/macPronalts.swift
@@ -1,7 +1,29 @@
 //input
 var numberOfRepetition:Int = Int(readLine()!)!
 
+var total:Double = 0
+
 //utilizando for 
 for _ in 1...numberOfRepetition{
-    
+    //inputs
+    let id:Int = Int(readLine()!)!
+    let quantity:Int = Int(readLine()!)!
+
+    switch id {
+        case 1001:
+            total = total + (Double(quantity) * 1.50)
+        case 1002:
+            total = total + (Double(quantity) * 2.50)
+        case 1003:
+            total = total + (Double(quantity) * 3.50)
+        case 1004:
+            total = total + (Double(quantity) * 4.50)
+        case 1005:
+            total = total + (Double(quantity) * 5.50)
+        default:
+            print("invalid ID")
+    }
 }
+
+//output
+print(total)
