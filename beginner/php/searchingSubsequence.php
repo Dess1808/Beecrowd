bfe47be28e87ef84efc70ee2107ab275d2d66584
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2025-03-27 14:49:51 -0400

    alguns commits atrasados

---
+++ beginner/php/searchingSubsequence.php
@@ -0,0 +1,9 @@
+<?php
+$string1 = "23112121222";
+$tofind = "1";
+
+//preciso encontrar um jeito de particionar essa string de forma dinamica
+
+for ($i = 0; $i < 10; $i++) {
+   
+};
---
+++ beginner/python/searching.py
@@ -0,0 +1,13 @@
+#bloco try/except para o EOF
+
+#show() fuction
+
+#como fazer essa busca em numero gigante
+
+#hipotoese 1 - blocos if encadeados?
+
+#fazer multiplas contagens, continuar checando enquanto o numero na sequencia for igual.
+#comparar strings
+
+    
+
