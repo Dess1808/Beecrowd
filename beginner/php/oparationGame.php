4b06f725e9e9e0c5a885432276eeb3d3d2eb33e0
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-04-16 13:16:26 -0400

    continuar..

--- beginner/php/oparationGame.php
+++ beginner/php/oparationGame.php
@@ -1,3 +1,37 @@
 <?php
 //substituição de caracter em string
 //teste fsfsdfds
+/*
+    one class (Operation)
+    lot of methods 
+
+    DRY - don't repeat yoursel
+
+    input checks
+    //obs: a primeira expressão preciso encontrar o unico espaco em branco e alterar para o simbolo
+    da "operação"
+
+    //obs: a respota deve ser dividida nas partes "nome", "seleção da operação" e "simbolo operação"
+*/
+
+class Operation {
+    private $expressionInputUser = [];
+    private $nameUser;
+    private $opSelection;
+    private $operation;
+    
+    public function __constructor($expressionInputUser, $answer = []){
+        $this->expressionInputUser = $expressionInputUser;
+        $this->nameUser = $answer[0];
+        $this->opSelection = $answer[1];
+        $this->operation = $answer[2];
+    }
+
+    public function getResults(): void{
+    
+    }
+
+    private function checkOperation($expression, $nameUser, $opSelection, $operation): string{
+        
+    }
+}
