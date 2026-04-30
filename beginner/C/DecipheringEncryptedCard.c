5261b4e832b12904c805d03c1b6c8d27159c20ac
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-04-30 14:48:59 -0400

    continuar codigo cipher

--- beginner/C/DecipheringEncryptedCard.c
+++ beginner/C/DecipheringEncryptedCard.c
@@ -1,23 +1,54 @@
 #include<stdio.h>
+#include<string.h>
+
+void clear_buffer();
 
 int main(void)
 {
-    //control variables
-    int c_inze = 0;
-    int numb_phrases = 0;
+    //variabels default
+    int size_cipher = 0;
+    int amount_phrases = 0;
+    char phrases[1000][100];
 
-    //vectors
-    char ciphers1[21];
-    char ciphers2[21];
-    char phrases[1000][1000];
+    //input 
+    scanf("%d %d", &size_cipher, &amount_phrases);
+    clear_buffer();
 
-    //input c_inze and numb_phrases
-    scanf("%d%d", &c_inze, &numb_phrases);
+    //implementar entrada da palavras cifradas!
+    char cipher1[size_cipher];
+    char cipher2[size_cipher];
 
-    //input ciphers 1&2
-    scanf("%s", ciphers1);
-    scanf("%s", ciphers2);
+    //input cipher1
+    fgets(cipher1, size_cipher, stdin);
+    cipher1[strcspn(cipher1, "\n")] = '\0';
 
-    //input phrases encrypted
-    
+    //input cipher2
+    fgets(cipher2, size_cipher, stdin);
+    cipher2[strcspn(cipher2, "\n")] = '\0';
+
+    //input phrases
+    for (int i = 0; i < amount_phrases; i++)
+    {
+        fgets(phrases[i], sizeof(phrases[i]), stdin);
+
+        //rmv \n
+        phrases[i][strcspn(phrases[i], "\n")] = '\0';
+    }
+
+    putchar('\n');
+
+    //output
+    printf("output..\n");
+    for (int i = 0; i < amount_phrases; i++)
+    {
+        printf("%s\n", phrases[i]);
+    }
+
+    return 0;
+}
+
+void clear_buffer()
+{
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
 }
--- beginner/C/debug.c
+++ beginner/C/debug.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
+#include<string.h>
 
 int main()
 {
-    int numb_phrases = 2;
-    char phrases[2][10];
-
-    //verificar por que não no laço for!!
-    //input
-    for (int i = 0; i < numb_phrases; i++)
-    {
-        for (int j = 0; j < numb_phrases; j++)
-        {   
-            scanf("%s", phrases[i]);
-        }
-    }
-
-    //output
-    for (int i = 0; i < numb_phrases; i++)
-    {
-        printf("%s\n", phrases[i]);
-    }
-
-    return 0;
 }
