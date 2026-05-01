a91662285a03320659b04a2602c5eb1118d1e2f4
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-05-01 19:32:55 -0400

    logica funcionando, implementar no problema main

--- beginner/C/DecipheringEncryptedCard.c
+++ beginner/C/DecipheringEncryptedCard.c
@@ -1,48 +1,77 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 
 void clear_buffer();
 
 int main(void)
 {
-    //variabels default
-    int size_cipher = 0;
-    int amount_phrases = 0;
-    char phrases[1000][100];
+    // //variabels default
+    // int size_cipher = 0;
+    // int amount_phrases = 0;
+    // char phrases[1000][100];
 
-    //input 
-    scanf("%d %d", &size_cipher, &amount_phrases);
-    clear_buffer();
+    // //input 
+    // scanf("%d %d", &size_cipher, &amount_phrases);
+    // clear_buffer();
 
-    //implementar entrada da palavras cifradas!
-    char cipher1[size_cipher];
-    char cipher2[size_cipher];
+    // //implementar entrada da palavras cifradas!
+    // char cipher1[size_cipher];
+    // char cipher2[size_cipher];
 
     //input cipher1
-    fgets(cipher1, size_cipher, stdin);
-    cipher1[strcspn(cipher1, "\n")] = '\0';
+    /*
+        1 - identificar cipher1, alterar mesma posicao no cipher2
+        2 - identificar cipher2, alterar na mesma posicao de cipher1 
+    */
 
-    //input cipher2
-    fgets(cipher2, size_cipher, stdin);
-    cipher2[strcspn(cipher2, "\n")] = '\0';
+    //input test
 
     //input phrases
-    for (int i = 0; i < amount_phrases; i++)
-    {
-        fgets(phrases[i], sizeof(phrases[i]), stdin);
+    // for (int i = 0; i < amount_phrases; i++)
+    // {
+    //     fgets(phrases[i], sizeof(phrases[i]), stdin);
 
-        //rmv \n
-        phrases[i][strcspn(phrases[i], "\n")] = '\0';
+    //     //rmv \n
+    //     phrases[i][strcspn(phrases[i], "\n")] = '\0';
+    // }
+
+
+    //logic problem
+    char cipher1[6] = {'Z', 'E', 'N', 'I', 'T', '\0'}; 
+    char cipher2[6] = {'P', 'O', 'L', 'A', 'R', '\0'};
+    char phrases[100] = {'O', 's', 'r', 'o', ' ', 'r', 'o', 'x', 'r', 'e', ' ', 'o', 's', 'r', 'i', '\0'};
+
+    //cipher1
+    for (int i = 0; phrases[i] != '\0'; i++)
+    {
+        //posso utilizar o tamanho da word cipher inserido pelo usuario
+        for (int j = 0; cipher1[j] != '\0'; j++)
+        {
+            //cipher1 verification
+            if (cipher1[j] == phrases[i])
+            {
+                phrases[i] = cipher2[j];
+            }
+            else if (tolower(cipher1[j]) == phrases[i])
+            {
+                phrases[i] = tolower(cipher2[j]);
+            }
+            else if (cipher2[j] == phrases[i]) //cipher2 verification
+            {
+                phrases[i] = cipher1[j];
+            }
+                
+            else if (phrases[i] == tolower(cipher2[j]))
+            {
+                phrases[i] = tolower(cipher1[j]);
+            }        
+        }
     }
 
-    putchar('\n');
 
     //output
-    printf("output..\n");
-    for (int i = 0; i < amount_phrases; i++)
-    {
-        printf("%s\n", phrases[i]);
-    }
+    printf("%s\n", phrases);
 
     return 0;
 }
