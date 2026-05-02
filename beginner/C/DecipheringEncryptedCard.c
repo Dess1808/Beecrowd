676f2e32249a269b72167597f80aa238885a46ee
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-05-01 21:13:52 -0400

    finalizado, falta subir para o beecrowd

--- beginner/C/DecipheringEncryptedCard.c
+++ beginner/C/DecipheringEncryptedCard.c
@@ -6,72 +6,85 @@ void clear_buffer();
 
 int main(void)
 {
-    // //variabels default
-    // int size_cipher = 0;
-    // int amount_phrases = 0;
-    // char phrases[1000][100];
+    //variabels default
+    int size_cipher = 0;
+    int amount_phrases = 0;
+    char phrases[1000][100];
 
-    // //input 
-    // scanf("%d %d", &size_cipher, &amount_phrases);
-    // clear_buffer();
+    //input 
+    scanf("%d %d", &size_cipher, &amount_phrases);
+    clear_buffer();
 
-    // //implementar entrada da palavras cifradas!
-    // char cipher1[size_cipher];
-    // char cipher2[size_cipher];
+    //implementar entrada da palavras cifradas!
+    char cipher1[size_cipher+1];
+    char cipher2[size_cipher+1];
 
     //input cipher1
-    /*
-        1 - identificar cipher1, alterar mesma posicao no cipher2
-        2 - identificar cipher2, alterar na mesma posicao de cipher1 
-    */
-
-    //input test
+    fgets(cipher1, sizeof(cipher1), stdin);
+    cipher1[strcspn(cipher1, "\n")] = '\0';
+    clear_buffer();
+    
+    //input cipher2
+    fgets(cipher2, sizeof(cipher2), stdin);
+    cipher2[strcspn(cipher2, "\n")] = '\0';
+    clear_buffer();
 
     //input phrases
-    // for (int i = 0; i < amount_phrases; i++)
-    // {
-    //     fgets(phrases[i], sizeof(phrases[i]), stdin);
-
-    //     //rmv \n
-    //     phrases[i][strcspn(phrases[i], "\n")] = '\0';
-    // }
-
-
-    //logic problem
-    char cipher1[6] = {'Z', 'E', 'N', 'I', 'T', '\0'}; 
-    char cipher2[6] = {'P', 'O', 'L', 'A', 'R', '\0'};
-    char phrases[100] = {'O', 's', 'r', 'o', ' ', 'r', 'o', 'x', 'r', 'e', ' ', 'o', 's', 'r', 'i', '\0'};
-
-    //cipher1
-    for (int i = 0; phrases[i] != '\0'; i++)
+    for (int i = 0; i < amount_phrases; i++)
     {
-        //posso utilizar o tamanho da word cipher inserido pelo usuario
-        for (int j = 0; cipher1[j] != '\0'; j++)
-        {
-            //cipher1 verification
-            if (cipher1[j] == phrases[i])
-            {
-                phrases[i] = cipher2[j];
-            }
-            else if (tolower(cipher1[j]) == phrases[i])
-            {
-                phrases[i] = tolower(cipher2[j]);
-            }
-            else if (cipher2[j] == phrases[i]) //cipher2 verification
-            {
-                phrases[i] = cipher1[j];
-            }
-                
-            else if (phrases[i] == tolower(cipher2[j]))
-            {
-                phrases[i] = tolower(cipher1[j]);
-            }        
-        }
+        fgets(phrases[i], sizeof(phrases[i]), stdin);
+
+        //rmv \n
+        phrases[i][strcspn(phrases[i], "\n")] = '\0';
     }
 
 
+    //logic problem
+
+    //select phrase
+    for (int i = 0; i < amount_phrases; i++)
+    {
+        //running itself phrases
+        for (int j = 0; phrases[i][j] != '\0'; j++)
+        {       
+            //verification cipher lettera
+            for (int k = 0; k < size_cipher; k++)
+            {
+                //cipher1 verification
+                if (cipher1[k] == phrases[i][j])
+                {
+                    //uppercase default
+                    phrases[i][j] = cipher2[k];
+                    break;
+                }
+                else if (tolower(cipher1[k]) == phrases[i][j])
+                {
+                    phrases[i][j] = tolower(cipher2[k]);
+                    break;
+                }
+                else if (cipher2[k] == phrases[i][j]) //cipher2 verification
+                {      
+                    //uppercase default
+                    phrases[i][j] = cipher1[k];
+                    break;
+                }
+                else if (tolower(cipher2[k]) == phrases[i][j])
+                {
+                    phrases[i][j] = tolower(cipher1[k]);
+                    break;
+                }
+            }
+        }
+    }
+
     //output
-    printf("%s\n", phrases);
+    for (int i = 0; i < amount_phrases; i++)
+    {
+        printf("%s\n", phrases[i]);
+    }
+    
+    putchar('\n');
+    putchar('\n');
 
     return 0;
 }
